d97e35b00876bdf5334d69141ed4360f673b43c6
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-08-05 15:26:23 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,12 @@
+for num in range (10,0,-1):
+ print(num)
+print(".............................")
+for num in range(2,51):
+        for i in range(2,num):
+            if num % i==0:
+                break
+            else:
+print(num,end=" ")
+print("\n..........")
+ squares = [num**2 for num in range(1,11)]
+         print(squaress
