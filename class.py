78b17fd7d7658cbc7f3b90e861a4071bca6fccf3
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-08-26 00:23:50 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,54 @@
+# class Employee:
+#   name="akshat"
+#   language="py"
+#   salary=12000
+  
+  
+# akshat=Employee()
+
+# print(akshat.name,akshat.language)
+
+
+
+# class Employee:
+#   name="akshat"
+#   language="python" # this is my class attribute
+#   salary=120000
+  
+# abcd=Employee()
+# abcd.language="javascript" # this is instance attribute
+
+# print(abcd.name,abcd.language,abcd.salary)
+
+
+class Employee:
+  name="akshat"
+  language="python"
+  salary=1200000
+  
+  def getinfo(self):
+     print(f"the languaage is : {self.language} and salary is {self.salary}" )
+  
+abcd=Employee()
+
+abcd.language="javascript"
+
+# print(abcd.name,abcd.language,abcd.salary)
+abcd.getinfo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
