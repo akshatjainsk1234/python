6572307ea73d8b90a5f9458a7a7ae7b1144db87f
Akshat Jain <97946554+akshatjainsk1234@users.noreply.github.com>
2024-08-26 00:24:50 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,161 @@
+# n=int(input("enter your number"))
+
+# for i in range(1,n+1):
+#   print(" "* (n-1),end="" )
+#   print("*"* (2*i-1),end="")
+#   print("\n")
+
+
+
+
+
+
+
+# n=int(input("enter your table"))
+
+# for i in range(1,11):
+#   print(f"{n}*{11-i}={n*(i-11)}")
+
+
+# a=25
+# b=25
+# c=10
+
+# average=(a+b+c)/3
+# 
+# print(average)
+
+
+# def avg():
+#   a=int(input("enter your number 1:"))
+#   b=int(input("enter your number 2:"))
+#   c=int(input("enter your number 3:"))
+#   average=(a+b+c)/3
+#   print(average)
+  
+# avg()
+  
+
+# def number():
+#   num=int(input("enter your number :"))
+  
+#   if(num%2)==0:
+#     print("even")
+#   else:
+#     print("odd")
+
+# number()
+
+# def goodday(name):
+#   print("good day " + name)
+  
+# goodday("Shaily")
+
+
+# def avg():
+#   a=int(input("enter number 1"))
+#   b=int(input("enter number 2"))
+#   c=int(input("enter number 3"))
+#   average=(a+b+c)/3
+#   return average
+# d=avg()
+# print(d)
+
+  
+# def goodday(name,ending="thankyou"):
+#   print("goodday " + name)
+#   print(ending)
+# goodday("shaily")
+  
+
+
+# def factorial(n):
+#   if(n==1 or n==0):
+#     return 1
+#   return n*factorial(n-1)
+# n=int(input("enter your number"))
+# print(f"The factorial is : {factorial(n)}")
+
+
+# def greatest(a,b,c):
+#   if(a>b and a>c):
+#     return a
+#   elif(b>a and b>c):
+#     return b
+#   elif(c>b and c>a):
+#     return c
+  
+# a=1
+# b=2
+# c=4
+# print(greatest(a,b,c))
+  
+# f=int(input("enter your temperature"))
+# c=5*(f-32)/9
+# # print(c)
+
+# def f_to_c(f):
+#   return 5*(f-32)/9
+# f=int(input("enter number"))
+# print(f_to_c(f))
+  
+# def sum(n):
+#   if(n==1):
+#     return 1
+#   return sum(n-1)+n
+# print(sum(4))
+
+    
+# def pattern(n):
+#   if(n==0):
+#     return 
+#   print("*" * n)
+#   pattern(n-1)
+  
+# pattern(2)
+
+
+# def inch_to_cm(inch):
+#   return inch*2.54
+# n=int(input("enter your number"))
+# print(f"the value is {inch_to_cm(n)}")
+
+
+# def multiply(n):
+#     for i in range(1,11):
+#       print(f"{n}*{i}={n*i}")
+    
+# multiply(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
